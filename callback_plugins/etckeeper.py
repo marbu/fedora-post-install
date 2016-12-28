f5bc2ad8ab27525c3f86c7f438847223b0777b0b
Martin Bukatovic <martin.bukatovic@gmail.com>
2016-12-28 03:36:10 +0100

    etckeeper plugin: require whitelisting
    So that it's easy to disable the plugin (eg. when running
    user_martin.yml playbook under non root user account without any need to
    use etckeeper).

--- callback_plugins/etckeeper.py
+++ callback_plugins/etckeeper.py
@@ -59,7 +59,7 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = None # TODO: specify the type properly
     CALLBACK_NAME = 'etckeeper'
-    CALLBACK_NEEDS_WHITELIST = False
+    CALLBACK_NEEDS_WHITELIST = True
 
     def __init__(self):
         super(CallbackModule, self).__init__()
